87e02849929456576e680bc9e2c0e609a5ef24f4
Patricio Agustín Jimenez Paz <patojimenezpaz@gmail.com>
2022-05-23 23:45:15 -0300

    Finalizada Calculadora V1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,23 +1,62 @@
-﻿Console.WriteLine("Ingrese un número para invertirlo: ");
-string entrada = Console.ReadLine();
+﻿do {
+    Console.Clear();
 
-int number = 0;
+    Console.WriteLine("====== Calculadora V1 ======");
+    Console.WriteLine("1) Sumar\n2) Restar\n3) Multiplicar\n4) Dividir");
 
-if (Int32.TryParse(entrada, out number)) {
-    if (number > 0) {
-        int inverted = 0, ultimoDigito = 0;
+    string entrada = Console.ReadLine();
+    int operacion = 0;
 
-        while (number > 0) {
-            ultimoDigito = number % 10;
-            inverted = inverted * 10 + ultimoDigito;
-            number /= 10;
-        }
-
-        Console.WriteLine("Número invertido: " + inverted);
-    } else {
-        Console.WriteLine("El número ingresado es menor que cero. No se invertirá el número");
+    while (!Int32.TryParse(entrada, out operacion) || operacion < 1 || operacion > 4) {
+        Console.WriteLine("Error! Debe ingresar una opción válida");
+        entrada = Console.ReadLine();
     }
-} else {
-    Console.WriteLine("Error! Debe ingresar un número");
+
+    double num1 = 0, num2 = 0;
+    Console.Write("Número 1: ");
+    entrada = Console.ReadLine();
+    while (!Double.TryParse(entrada, out num1)) {
+        Console.WriteLine("Error! Debe ingresar un número entero");
+        entrada = Console.ReadLine();
+    }
+
+    Console.Write("Número 2: ");
+    entrada = Console.ReadLine();
+    while (!Double.TryParse(entrada, out num2)) {
+        Console.WriteLine("Error! Debe ingresar un número entero");
+        entrada = Console.ReadLine();
+    }
+
+    switch (operacion) {
+        case 1:
+            Console.WriteLine("Resultado: " + sumar(num1, num2));
+            break;
+        case 2:
+            Console.WriteLine("Resultado: " + restar(num1, num2));
+            break;
+        case 3:
+            Console.WriteLine("Resultado: " + multiplicar(num1, num2));
+            break;
+        case 4:
+            Console.WriteLine("Resultado: " + dividir(num1, num2));
+            break;
+    }
+
+    Console.WriteLine("Desea realizar otra operación?\n0. No\n1. Si");
+} while (Console.ReadKey(false).Key != ConsoleKey.D0);
+
+double sumar(double num1, double num2) {
+    return num1 + num2;
 }
 
+double restar(double num1, double num2) {
+    return num1 - num2;
+}
+
+double multiplicar(double num1, double num2) {
+    return num1 * num2;
+}
+
+double dividir(double num1, double num2) {
+    return num1 / num2;
+}
