490c91923db0ed48c86f12a92d60bde0e623f043
Patricio Agustín Jimenez Paz <patojimenezpaz@gmail.com>
2022-05-23 22:26:50 -0300

    Ejercicio1 realizado

---
+++ MyApp/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Ingrese un número para invertirlo: ");
+string entrada = Console.ReadLine();
+
+int number = 0;
+
+if (Int32.TryParse(entrada, out number)) {
+    if (number > 0) {
+        int inverted = 0, ultimoDigito = 0;
+
+        while (number > 0) {
+            ultimoDigito = number % 10;
+            inverted = inverted * 10 + ultimoDigito;
+            number /= 10;
+        }
+
+        Console.WriteLine("Número invertido: " + inverted);
+    } else {
+        Console.WriteLine("El número ingresado es menor que cero. No se invertirá el número");
+    }
+} else {
+    Console.WriteLine("Error! Debe ingresar un número");
+}
+
